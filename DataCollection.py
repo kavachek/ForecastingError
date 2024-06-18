cff91be76cb03d253e674d07ddfd599a08fdb8f7
kavac <kavachek2@gmail.com>
2024-06-19 01:44:08 +0300

    Были добавлены нули между разными ошибка, учитывая разницу между днями. Были добавлены дополнительные дни между зависимыми днями с ошибками.

--- DataCollection.py
+++ DataCollection.py
@@ -10,9 +10,16 @@ class StartEndIndex:
         # Список для дат, не содержащий в себе объект даты.
         self.list_date_collection_not_datatime = []
         # Список для дат, содержащий в себе объект даты.
+        self.list_date_collection_datatime_not_add_date = []
+        # Новый список для хранения всех дат, включая промежуточные.
+        self.list_date_collection_datatime_add_date = []
         self.list_date_collection_datatime = []
         # Список для ошибок.
         self.list_error_collection = []
+        # Список для ошибок с нулями.
+        self.numbers_with_zeros = []
+        # Список для ошибок без нулей.
+        self.numbers_with_not_zeros = []
         # Список для индекса дат.
         self.list_index_date = [0]
         # Настройки.
@@ -21,8 +28,8 @@ class StartEndIndex:
         self.next_index = None
         self.current_date_str = None
 
-        self.list_date_server = ['01.02.2024', '05.02.2024', '09.02.2024', '11.02.2024', '14.02.2024', '15.02.2024',
-                                 '19.02.2024', '23.02.2024']
+        self.list_date_server = ['01.02.2024', '03.02.2024', '08.02.2024', '11.02.2024', '14.02.2024', '15.02.2024',
+                                 '19.02.2024', '23.02.2024']  # [3, 3, 1, 2, 0, 4, 4]
         self.list_error_server = ['1', '2', '3', '2', '5', '6', '7', '8']
 
         # self.list_date_server = []
@@ -62,30 +69,61 @@ class StartEndIndex:
                 print('Даты и ошибки не совпадают.')
 
     def difference_date(self):
-        # Проверка, что self.list_date_collection содержит объекты datetime
+        # Проверка, что self.list_date_collection_not_datatime содержит объекты datetime.
         if isinstance(self.list_date_collection_not_datatime[0], str):
             self.list_date_collection_not_datatime = [datetime.strptime(date, '%d.%m.%Y')
                                                       for date in self.list_date_collection_not_datatime]
-        # Вычисление разницы между каждой парой последовательных дат и их порядковых индексов
+        # Новый список для хранения всех дат, включая промежуточные.
+        all_dates = []
+        # Добавляем промежуточные даты.
+        for i in range(len(self.list_date_collection_not_datatime) - 1):
+            start_date = self.list_date_collection_not_datatime[i]
+            end_date = self.list_date_collection_not_datatime[i + 1]
+            current_date = start_date
+            while current_date <= end_date:
+                all_dates.append(current_date)
+                current_date += timedelta(days=1)
+        # Преобразуем объекты datetime обратно в строки.
+        self.list_date_collection_datatime = [date.strftime("%d.%m.%Y") for date in all_dates]
+        # Цикл нужен, чтобы сохранить в правильном формате даты в список list_date_collection_datatime_not_add_date.
+        for date in range(len(self.list_date_collection_datatime)):
+            full_date = self.list_date_collection_datatime[date]
+            # Сохраняю даты в список.
+            self.current_date_str = full_date
+            self.list_date_collection_datatime_not_add_date.append(self.current_date_str)
+        # Цикл нужен для удаления дубликатов дат.
+        for removal_of_chemicals in self.list_date_collection_datatime_not_add_date:
+            if removal_of_chemicals not in self.list_date_collection_datatime_add_date:
+                self.list_date_collection_datatime_add_date.append(removal_of_chemicals)
+
+        # Вычисление разницы между каждой парой последовательных дат и их порядковых индексов.
         base_date = self.list_date_collection_not_datatime[0]
         base_index = 0
-        for date in range(len(self.list_date_collection_not_datatime)):
-            full_date = self.list_date_collection_not_datatime[date]
-            # Сохраняю даты в список.
-            self.current_date_str = full_date.strftime("%d.%m.%Y")
-            self.list_date_collection_datatime.append(self.current_date_str)
-
+        add_zero = []
         for index in range(len(self.list_date_collection_not_datatime) - 1):
             current_date = self.list_date_collection_not_datatime[index]
             next_date = self.list_date_collection_not_datatime[index + 1]
             delta_days = (next_date - current_date).days
+            # Добавляем получившиеся значения в список.
+            add_zero.append(delta_days)
             current_index = base_index + (current_date - base_date).days
             next_index = current_index + delta_days
             # Сохраняю индекс дат в список.
             self.next_index = next_index
             self.list_index_date.append(self.next_index)
-            # print(f'Дата: {self.current_date_str} - {next_date.strftime("%d.%m.%Y")}:'
-            #       f' разница {delta_days} дней, Порядковый индекс: {self.next_index}')
+
+        # Вычитаем по 1 единицы из каждого значения.
+        for subtract_one in add_zero:
+            subtract_one -= 1
+            self.numbers_with_not_zeros.append(subtract_one)
+        # Цикл для добавления чисел и нулей
+        for i in range(len(self.list_error_collection) - 1):
+            current_number = self.list_error_collection[i]
+            self.numbers_with_zeros.append(current_number)
+            # Используем значение из списка self.numbers_with_not_zeros для добавления нулей
+            self.numbers_with_zeros.extend([0] * self.numbers_with_not_zeros[i])
+        # Добавляем последний элемент только один раз после завершения цикла
+        self.numbers_with_zeros.append(self.list_error_collection[-1])
 
 
 class MinDate(StartEndIndex):
@@ -115,12 +153,16 @@ class Info(MinDate):
 
     def collection_data(self):
         # НУЖНО БУДЕТ ДОРАБОТАТЬ ErrorDuration!!!!!!!!!!!!!!!!!!!!!
-        for code_error, date_index, month_date in zip(self.list_error_collection, self.list_index_date,
-                                                      self.list_date_collection_datatime):
+        print(self.list_date_collection_datatime_add_date)
+        for code_error, date_index, month_date in zip(self.numbers_with_zeros, self.list_index_date,
+                                                      self.list_date_collection_datatime_add_date):
             self.collecting_list_info.append({'Code': code_error,
                                               'Found': [{'TimeIndex': date_index,
                                                          'Month': month_date,
                                                          'ErrorDuration': 1}]})
+        for i in self.collecting_list_info:
+            print(i)
+
 
     def collection_data_run(self):
         code_error = []
