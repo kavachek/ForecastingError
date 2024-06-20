20468d27e0507031929c835bbc46759a5a6eb925
kavac <kavachek2@gmail.com>
2024-06-20 20:13:52 +0300

    Полностью доделан класс Info из файла DataCollection.py. В будущем буде необходимо доработать ErrorDuration и составить небольшую документацию для каждой функции и классов.

--- DataCollection.py
+++ DataCollection.py
@@ -1,4 +1,5 @@
 import requests
+from itertools import zip_longest
 from Text import Error_200, Date_error, Date_range, Process_error
 from datetime import datetime, timedelta
 from flask import Flask, request, jsonify
@@ -18,10 +19,10 @@ class StartEndIndex:
         self.list_error_collection = []
         # Список для ошибок с нулями.
         self.numbers_with_zeros = []
-        # Список для ошибок без нулей.
-        self.numbers_with_not_zeros = []
+        # Дистанция с нулями.
+        self.distance_numbers_with_zeros = []
         # Список для индекса дат.
-        self.list_index_date = [0]
+        self.list_index_date = [1]
         # Настройки.
         self.setup_routes()
         # Атрибуты класса.
@@ -29,8 +30,8 @@ class StartEndIndex:
         self.current_date_str = None
 
         self.list_date_server = ['01.02.2024', '03.02.2024', '08.02.2024', '11.02.2024', '14.02.2024', '15.02.2024',
-                                 '19.02.2024', '23.02.2024']  # [3, 3, 1, 2, 0, 4, 4]
-        self.list_error_server = ['1', '2', '3', '2', '5', '6', '7', '8']
+                                 '19.02.2024', '23.02.2024']
+        self.list_error_server = ['1', '2', '3', '4', '5', '1', '7', '5']
 
         # self.list_date_server = []
         # self.list_error_server = []
@@ -76,21 +77,23 @@ class StartEndIndex:
         # Новый список для хранения всех дат, включая промежуточные.
         all_dates = []
         # Добавляем промежуточные даты.
-        for i in range(len(self.list_date_collection_not_datatime) - 1):
-            start_date = self.list_date_collection_not_datatime[i]
-            end_date = self.list_date_collection_not_datatime[i + 1]
+        for add_dates in range(len(self.list_date_collection_not_datatime) - 1):
+            start_date = self.list_date_collection_not_datatime[add_dates]
+            end_date = self.list_date_collection_not_datatime[add_dates + 1]
             current_date = start_date
             while current_date <= end_date:
                 all_dates.append(current_date)
                 current_date += timedelta(days=1)
         # Преобразуем объекты datetime обратно в строки.
         self.list_date_collection_datatime = [date.strftime("%d.%m.%Y") for date in all_dates]
+
         # Цикл нужен, чтобы сохранить в правильном формате даты в список list_date_collection_datatime_not_add_date.
         for date in range(len(self.list_date_collection_datatime)):
             full_date = self.list_date_collection_datatime[date]
             # Сохраняю даты в список.
             self.current_date_str = full_date
             self.list_date_collection_datatime_not_add_date.append(self.current_date_str)
+
         # Цикл нужен для удаления дубликатов дат.
         for removal_of_chemicals in self.list_date_collection_datatime_not_add_date:
             if removal_of_chemicals not in self.list_date_collection_datatime_add_date:
@@ -101,11 +104,14 @@ class StartEndIndex:
         base_index = 0
         add_zero = []
         for index in range(len(self.list_date_collection_not_datatime) - 1):
+            # Текущая дата.
             current_date = self.list_date_collection_not_datatime[index]
             next_date = self.list_date_collection_not_datatime[index + 1]
+            # Разница между днями.
             delta_days = (next_date - current_date).days
             # Добавляем получившиеся значения в список.
             add_zero.append(delta_days)
+            # self.numbers_with_not_zeros.append(delta_days)
             current_index = base_index + (current_date - base_date).days
             next_index = current_index + delta_days
             # Сохраняю индекс дат в список.
@@ -115,13 +121,14 @@ class StartEndIndex:
         # Вычитаем по 1 единицы из каждого значения.
         for subtract_one in add_zero:
             subtract_one -= 1
-            self.numbers_with_not_zeros.append(subtract_one)
+            self.distance_numbers_with_zeros.append(subtract_one)
+
         # Цикл для добавления чисел и нулей
         for i in range(len(self.list_error_collection) - 1):
             current_number = self.list_error_collection[i]
             self.numbers_with_zeros.append(current_number)
             # Используем значение из списка self.numbers_with_not_zeros для добавления нулей
-            self.numbers_with_zeros.extend([0] * self.numbers_with_not_zeros[i])
+            self.numbers_with_zeros.extend([0] * self.distance_numbers_with_zeros[i])
         # Добавляем последний элемент только один раз после завершения цикла
         self.numbers_with_zeros.append(self.list_error_collection[-1])
 
@@ -135,10 +142,11 @@ class MinDate(StartEndIndex):
         if not self.list_error_collection:
             print('Нет ошибок для обработки.')  # Process_error
 
+        # Минимальная дата для ошибки.
         min_error = min(self.list_error_collection)
         min_date_index = self.list_error_collection.index(min_error)
+        # Самая первая дата ошибки.
         corresponding_date = self.list_date_server[min_date_index]
-        # print(f"минимальная ошибка: {min_error}")
         # print(f"самая первая дата ошибки: {corresponding_date}")
 
 
@@ -150,19 +158,26 @@ class Info(MinDate):
         self.collecting_list_info = []
         self.difference_date()
         self.collection_data()
+        # Переменная для сохранения последовательности шагов назад между датами.
+        self.run_before = []
+        self.interval_error_list = []
 
     def collection_data(self):
         # НУЖНО БУДЕТ ДОРАБОТАТЬ ErrorDuration!!!!!!!!!!!!!!!!!!!!!
-        print(self.list_date_collection_datatime_add_date)
-        for code_error, date_index, month_date in zip(self.numbers_with_zeros, self.list_index_date,
-                                                      self.list_date_collection_datatime_add_date):
+        # на всякий случай!
+        # for code_error, date_index, month_date in zip_longest(self.numbers_with_zeros, self.list_index_date,
+        #                                                       self.list_date_collection_datatime_add_date,
+        #                                                       fillvalue=None):
+        # на всякий случай!
+        for code_error, date_index, month_date in zip_longest(self.list_error_collection, self.list_index_date,
+                                                              self.list_date_server,
+                                                              fillvalue=None):
             self.collecting_list_info.append({'Code': code_error,
                                               'Found': [{'TimeIndex': date_index,
                                                          'Month': month_date,
                                                          'ErrorDuration': 1}]})
         for i in self.collecting_list_info:
             print(i)
-
 
     def collection_data_run(self):
         code_error = []
@@ -179,15 +194,30 @@ class Info(MinDate):
         for unique_code in unique_error_codes:
             indices_of_code = [index_code for index_code, code in enumerate(code_error) if code == unique_code]
 
-            for i in range(len(indices_of_code)-1):
-                start_index = indices_of_code[i]
-                end_index = indices_of_code[i + 1]
-                print(start_index, end_index)
+            # Цикл для определения начального и конечного индекса.
+            for current_index in range(len(indices_of_code)-1):
+                start_index = indices_of_code[current_index]
+                end_index = indices_of_code[current_index + 1]
+
+                # Вычисление разницы между конечным индексом и промежуточными индексами.
+                interval_differences = [error_indices_list[end_index] - index
+                                        for index in error_indices_list[start_index + 1:end_index]]
+                # Добавляем, получившиеся значения в список self.run_before.
+                self.run_before.append(interval_differences)
+
+                # Интервал между ошибками.
+                interval_error = code_error[start_index + 1:end_index]
+                self.interval_error_list.append(interval_error)
+
+                item_previous_errors = []
+                for intermediate_index in range(start_index + 1, end_index):
+                    if intermediate_index - start_index - 1 < len(self.run_before):
+                        temp_dict = {'Code': code_error[intermediate_index],
+                                     'StepBefore': self.run_before[intermediate_index - start_index - 1]}
+                        item_previous_errors.append(temp_dict)
 
 
-
-
-# Запуск сервера
+# Запуск сервера.
 if __name__ == '__main__':
     # Запускаем сервер для обработки POST запросов
     Server = StartEndIndex()
